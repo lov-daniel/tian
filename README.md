8bad8d5e87b97e453e9ba3bc70ac6f0096928cc0
lov-daniel <lovdaniel11@gmail.com>
2023-11-13 00:41:42 -0800

    setting up development platform

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# tian
+# T.I.A.N.
+
+What is T.I.A.N.[^1]
+-
+T.I.A.N. is an experimentation with machine learning, specifically object recognition and text recognition. The project is written entirely in Python and utilizing standard libraries such as NumPy and originally, the project had used a library known as PyTesseract[https://pypi.org/project/pytesseract/] in order to process the text, however, this felt like it was cheating, so I wanted to create and train my own OCR model instead of riding off of the Google Tesseract OCR engine.
+
+Applications of T.I.A.N.
+-
+A video game that I played a lot with my friends one summer was a game that had focused entirely on syllables. It was known as BombParty[https://jklm.fun/][^2]. I enjoyed the game a lot and wanted to ruin the fun for the people around me[^3]. I began to train the OCR model to recognize the syllables within the game and thus; the bot was born.
+
+Code in Action
+-
+(insert youtube video later)
+
+How does this all work?
+-
+
+
+
+### Foot Notes
+[^1]: T.I.A.N. stands for Text (and) Image Aggregation Neuron. This is also the name of my favorite stuffed animal. (see image)[]
+[^2]: In this game, you were given a syllable randomly and you had to provide a word that had contained this syllable. 
+[^3]: This is a joke, I love my friends (maybe).
--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-import numpy as np
+#General Libraries
+import numpy
+import pyautogui
+import time
+import mss
+from matplotlib import pyplot as plt
+
+#Image Processing Libraries
+from PIL import Image
+import pytesseract
+import cv2
+
+dimensions = pyautogui.size()
+
+
+
+# Obtained from MSS documention
+
+def imageProcessing(img, isolation):
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+
+    if isolation == True:
+        blur = cv2.GaussianBlur(img,(5,5),0)
+        threshold, img = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    return img
+
+def processSyllable(img):
+    return pytesseract.image_to_string(img)
+
+
+with mss.mss() as sct:
+    # Part of the screen to capture
+    monitorBounds = {"top": 0, "left": 0, "width": dimensions[0] , "height": dimensions[1]}
+    syllableBounds = {"top": round(dimensions[0] / 3.15), "left": round(dimensions[1] / 1.64), "width": 75, "height": 75}
+
+    while "Screen capturing":
+        last_time = time.time()
+
+        # Get raw pixels from the screen, save it to a Numpy array
+        syllableCapture = numpy.array(sct.grab(syllableBounds))
+
+        # Display the pictures
+        cv2.imshow("Syllable Capture", imageProcessing(syllableCapture, True))
+        print(f"fps: {1 / (time.time() - last_time)}")
+
+        # Press "q" to quit
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+            break
